1646a413070ca197c60a553c433973d5086752cd
Leon Organ <lrorgan@sep.com>
2024-03-15 21:23:59 -0400

    AI keeps fish inbounds

---
+++ Assets/Scripts/AI0.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class AI0 : MonoBehaviour
+{
+    public float StreamWidth = 100f;
+    public float SwimDepth = 30f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float xDelta = Mathf.Max(0, transform.localPosition.x - StreamWidth);
+        xDelta += Mathf.Min(0, transform.localPosition.x + StreamWidth);
+        float zDelta = Mathf.Max(0, transform.localPosition.z - SwimDepth);
+        zDelta += Mathf.Min(0, transform.localPosition.z + SwimDepth);
+
+        Debug.Log(name +":"+ transform.localPosition + " MoveX:" + xDelta + " MoveZ:" + zDelta);
+        transform.localPosition -= new Vector3(xDelta, 0, zDelta);
+    }
+}
