3eb36420e688a9deeef8d0000262865a6646f759
Clayton <cjdetke@yahoo.com>
2024-03-16 13:17:45 -0400

    Add front corner info to hazards

--- Assets/Scripts/Hazard.cs
+++ Assets/Scripts/Hazard.cs
@@ -5,6 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class Hazard : MonoBehaviour
 {
+    public Vector3 LeftFrontCorner;
+    public Vector3 RightFrontCorner;
+    public float FrontOffset;
+    public float LeftOffset;
+    public float RightOffset;
+
+    private void Start()
+    {
+        LeftFrontCorner = transform.position += new Vector3(LeftOffset, 0, FrontOffset);
+        RightFrontCorner = transform.position += new Vector3(RightOffset, 0, FrontOffset);
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
